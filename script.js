f5f60f99dfd00365298629fd2c6064049954fcf6
Sierra Cootes <sierrataryncootes@gmail.com>
2021-05-19 01:51:42 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    principal = Number(principal);
+    var rate = document.getElementById("rate").value;
+    rate = parseFloat(rate);
+    var years = document.getElementById("years").value;
+    years = Number(years);
+    var interest = principal * years * rate / 100;
+    if (validatePrincipal() === true) {
+        var currentDate = new Date();
+        var currentYear = currentDate.getFullYear();
+        currentYear = Number(currentYear);
+        var futureYear = currentYear + years;
+        var result = document.getElementById("result");
+        var output = "</br>If you deposit <mark>" + principal +"</mark>,<br/>" + "at an interest rate of <mark>" + rate + "</mark>%,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> in the year <mark>" + futureYear + "</mark>";
+        result.innerHTML = output;}
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+//fixed bug that came up with rate slider :)
+function validatePrincipal()
+{   var principal = document.getElementById("principal").value;
+    var errorMessage = 'Enter a positive number!'
+if ((principal < 0) || (principal == 0)) {
+     if (confirm(errorMessage)) {
+         document.getElementById("principal").focus();
+    }
+    } else {
+     return true;
+    }
+}
